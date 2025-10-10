7df9348a31d63ac73f05616e102d2c4d112def5d
zhengpj <598359997@qq.com>
2025-10-10 16:42:05 +0800

    音乐处理优化

--- Assets/Scripts/Manager/AudioManager.cs
+++ Assets/Scripts/Manager/AudioManager.cs
@@ -13,13 +13,13 @@ public class AudioManager : SingletonMono<AudioManager>
   [Range(0f, 1f)] public float sfxVolume = 1f;
 
   // 背景音乐
-  private AudioSource _bgmSource;
+  private AudioSource _bgm;
 
   // 正在播放的音效，包含循环音效
-  private readonly List<AudioSource> _sfxSources = new();
+  private readonly List<AudioSource> _sfxList = new();
 
   // 正在播放的循环音效
-  private readonly Dictionary<string, AudioSource> _activeSfx = new();
+  private readonly Dictionary<string, AudioSource> _loopSfxList = new();
 
   // 音效对象池
   private readonly Queue<AudioSource> _sfxPool = new();
@@ -29,15 +29,15 @@ public class AudioManager : SingletonMono<AudioManager>
   {
     base.Awake();
 
-    _bgmSource = gameObject.GetComponent<AudioSource>();
-    if (_bgmSource == null)
+    _bgm = gameObject.GetComponent<AudioSource>();
+    if (_bgm == null)
     {
-      _bgmSource = gameObject.AddComponent<AudioSource>();
+      _bgm = gameObject.AddComponent<AudioSource>();
     }
 
-    _bgmSource.loop = true;
-    _bgmSource.playOnAwake = false;
-    _bgmSource.volume = bgmVolume;
+    _bgm.loop = true;
+    _bgm.playOnAwake = false;
+    _bgm.volume = bgmVolume;
 
     // 初始化音效对象池
     for (var i = 0; i < InitialSfxPoolSize; i++)
@@ -49,6 +49,91 @@ public class AudioManager : SingletonMono<AudioManager>
     if (defaultBgm != null)
     {
       PlayBGM(defaultBgm);
+    }
+  }
+
+  // 暂停背景音乐和所有音效
+  public void Stop()
+  {
+    StopBGM();
+    StopAllSfx();
+  }
+
+  #region === BGM ===
+
+  public void PlayBGM(string soundType)
+  {
+    var clip = Resources.Load<AudioClip>("Sound/" + soundType);
+    PlayBGM(clip);
+  }
+
+  private void PlayBGM(AudioClip clip)
+  {
+    if (clip == null) return;
+    if (_bgm.clip == clip && _bgm.isPlaying) return;
+
+    StopAllCoroutines();
+    StartCoroutine(FadeInBGM(clip));
+  }
+
+  private void StopBGM(float fadeTime = 1f)
+  {
+    StopAllCoroutines();
+    StartCoroutine(FadeOutBGM(fadeTime));
+  }
+
+  private IEnumerator FadeInBGM(AudioClip clip, float fadeTime = 1f)
+  {
+    _bgm.clip = clip;
+    _bgm.volume = 0f;
+    _bgm.Play();
+
+    float t = 0f;
+    while (t < fadeTime)
+    {
+      t += Time.deltaTime;
+      _bgm.volume = Mathf.Lerp(0f, bgmVolume, t / fadeTime);
+      yield return null;
+    }
+
+    _bgm.volume = bgmVolume;
+  }
+
+  private IEnumerator FadeOutBGM(float fadeTime = 1f)
+  {
+    float startVol = _bgm.volume;
+    float t = 0f;
+    while (t < fadeTime)
+    {
+      t += Time.deltaTime;
+      _bgm.volume = Mathf.Lerp(startVol, 0f, t / fadeTime);
+      yield return null;
+    }
+
+    _bgm.Stop();
+    _bgm.clip = null;
+    _bgm.volume = bgmVolume;
+  }
+
+  #endregion =====================
+
+  #region === SFX ===
+
+  public void PlaySfx(string soundType, bool loop = false)
+  {
+    var clip = Resources.Load<AudioClip>("Sound/" + soundType);
+    PlaySfx(clip, soundType, loop);
+  }
+
+  // 停止播放循环音效
+  public void StopSfx(string soundType)
+  {
+    if (_loopSfxList.TryGetValue(soundType, out AudioSource src))
+    {
+      src.Stop();
+      _loopSfxList.Remove(soundType);
+      _sfxList.Remove(src);
+      ReleaseSfxSource(src);
     }
   }
 
@@ -79,91 +164,18 @@ public class AudioManager : SingletonMono<AudioManager>
     _sfxPool.Enqueue(src);
   }
 
-  // 暂停背景音乐和所有音效
-  public void Stop()
-  {
-    StopBGM();
-    StopAllSfx();
-  }
-
-  #region === BGM ===
-
-  public void PlayBGM(string soundType)
-  {
-    var clip = Resources.Load<AudioClip>("Sound/" + soundType);
-    PlayBGM(clip);
-  }
-
-  private void PlayBGM(AudioClip clip)
-  {
-    if (clip == null) return;
-    if (_bgmSource.clip == clip && _bgmSource.isPlaying) return;
-
-    StopAllCoroutines();
-    StartCoroutine(FadeInBGM(clip));
-  }
-
-  private void StopBGM(float fadeTime = 1f)
-  {
-    StopAllCoroutines();
-    StartCoroutine(FadeOutBGM(fadeTime));
-  }
-
-  private IEnumerator FadeInBGM(AudioClip clip, float fadeTime = 1f)
-  {
-    _bgmSource.clip = clip;
-    _bgmSource.volume = 0f;
-    _bgmSource.Play();
-
-    float t = 0f;
-    while (t < fadeTime)
-    {
-      t += Time.deltaTime;
-      _bgmSource.volume = Mathf.Lerp(0f, bgmVolume, t / fadeTime);
-      yield return null;
-    }
-
-    _bgmSource.volume = bgmVolume;
-  }
-
-  private IEnumerator FadeOutBGM(float fadeTime = 1f)
-  {
-    float startVol = _bgmSource.volume;
-    float t = 0f;
-    while (t < fadeTime)
-    {
-      t += Time.deltaTime;
-      _bgmSource.volume = Mathf.Lerp(startVol, 0f, t / fadeTime);
-      yield return null;
-    }
-
-    _bgmSource.Stop();
-    _bgmSource.clip = null;
-    _bgmSource.volume = bgmVolume;
-  }
-
-  #endregion =====================
-
-  #region === SFX ===
-
-  public void PlaySfx(string soundType, bool loop = false)
-  {
-    var clip = Resources.Load<AudioClip>("Sound/" + soundType);
-    PlaySfx(clip, soundType, loop);
-  }
-
   // ReSharper disable Unity.PerformanceAnalysis
   private void PlaySfx(AudioClip clip, string soundType, bool loop = false)
   {
-    if (clip == null) return;
-    if (loop && _activeSfx.ContainsKey(soundType)) return; // 如果已经有同类型的循环音效在播放，直接返回
+    if (!clip) return;
+    if (loop && _loopSfxList.ContainsKey(soundType)) return; // 如果已经有同类型的循环音效在播放，直接返回
 
     var sfx = AllocSfxSource();
     sfx.clip = clip;
     sfx.loop = loop;
     sfx.volume = sfxVolume;
     sfx.Play();
-    _sfxSources.Add(sfx); // 正在播放的音效
+    _sfxList.Add(sfx); // 正在播放的音效
 
     // 如果不是循环音效，则自动销毁
     if (!loop)
@@ -172,14 +184,14 @@ public class AudioManager : SingletonMono<AudioManager>
     }
     else
     {
-      _activeSfx.Add(soundType, sfx);
+      _loopSfxList.Add(soundType, sfx);
     }
   }
 
   private IEnumerator DestroyAfterPlay(AudioSource sfx)
   {
     yield return new WaitWhile(() => sfx.isPlaying);
-    _sfxSources.Remove(sfx);
+    _sfxList.Remove(sfx);
     ReleaseSfxSource(sfx);
   }
 
@@ -187,34 +199,26 @@ public class AudioManager : SingletonMono<AudioManager>
   private void StopAllSfx()
   {
     // 循环音效
-    foreach (var (key, src) in _activeSfx)
+    foreach (var (key, src) in _loopSfxList)
     {
       src.Stop();
-      _activeSfx.Remove(key);
-      _sfxSources.Remove(src);
+      _loopSfxList.Remove(key);
+      _sfxList.Remove(src);
       ReleaseSfxSource(src);
     }
 
+    _loopSfxList.Clear();
+
     // 全部音效
-    foreach (var sfx in _sfxSources.ToList())
+    foreach (var sfx in _sfxList.ToList())
     {
       if (sfx == null) continue;
       sfx.Stop();
-      _sfxSources.Remove(sfx);
+      _sfxList.Remove(sfx);
       ReleaseSfxSource(sfx);
     }
-  }
 
-  // 停止播放循环音效
-  public void StopSfx(string soundType)
-  {
-    if (_activeSfx.TryGetValue(soundType, out AudioSource src))
-    {
-      src.Stop();
-      _activeSfx.Remove(soundType);
-      _sfxSources.Remove(src);
-      ReleaseSfxSource(src);
-    }
+    _sfxList.Clear();
   }
 
   #endregion =====================
